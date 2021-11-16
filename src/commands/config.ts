f8c41339345541127a421dae9d4662dd65c2c411
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-11-16 08:54:55 -0500

    refactor: Prompt user for dangerous roles instead of blacklisting

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,4 +1,4 @@
-import {CommandInteraction, Role} from "discord.js";
+import {CommandInteraction, Role, MessageActionRow, MessageButton, Message} from "discord.js";
 import SlashCommand from "../SlashCommand";
 import axios from "axios";
 
@@ -21,18 +21,18 @@ export default class SetupCommand extends SlashCommand {
     ]);
   }
 
-  public async exec(i: CommandInteraction) {
-    switch (i.options.getSubcommand()) {
+  public async exec(int: CommandInteraction) {
+    switch (int.options.getSubcommand()) {
       case "verify_role": {
-        const role = i.options.getRole("role", true);
+        const role = int.options.getRole("role", true);
 
         if (!(role instanceof Role)) {
-          return i.reply(
+          return int.reply(
               "Required scope `bot` is missing., Re-add me with the scope and try again",
           );
         }
 
-        const botRoles = i.guild.me.roles;
+        const botRoles = int.guild.me.roles;
         const disallowedPermissions = [
           "ADMINISTRATOR",
           "KICK_MEMBERS",
@@ -44,43 +44,114 @@ export default class SetupCommand extends SlashCommand {
           "MANAGE_ROLES",
         ];
 
-        if (role.permissions.toArray().some((p) => disallowedPermissions.includes(p))) {
-          return i.reply(
-              `The role being applied must not have any of the following permissions. Remove them and try again.\n${
-              role.permissions.has("ADMINISTRATOR") ?
-                 "`ADMINISTRATOR`" :
-                 role.permissions
-                     .toArray()
-                     .filter((p) => disallowedPermissions.includes(p))
-                     .map((p) => `\`${p}\``)
-                     .join("\n")
-              }`,
-          );
-        }
-
         if (role.rawPosition > botRoles.highest.rawPosition) {
-          return i.reply({
+          return int.reply({
             content: `I cannot assign that role! Move or Create a new role under ${botRoles.highest.toString()}`,
             allowedMentions: {parse: []},
           });
         }
 
-        const req = (await axios.patch(`https://api.safecord.xyz/mongo/guild/${i.guild.id}`, {
-          verificationRole: role.id,
-        }, {
-          headers: {
-            "Content-Type": "application/json",
-            "authorization": process.env.BACKEND_API_KEY ?? '89aLG9EEsWKgTzZio1ZW',
-          },
-        })).data;
+        if (role.permissions.toArray().some((p) => disallowedPermissions.includes(p))) {
+          if (role.permissions.has("ADMINISTRATOR")) {
+            return int.reply(
+                `The role being applied must not have the \`ADMINISTRATOR\` permission. Remove it and try again.`,
+            );
+          }
+
+          const reply = await int.reply({
+            content: `The role being applied has some dangerous permissions.\n${role.permissions
+                .toArray()
+                .filter((p) => disallowedPermissions.includes(p))
+                .map((p) => `\`${p}\``)
+                .join("\n")}\nAre you sure you want to use this role?`,
+            components: [new MessageActionRow().addComponents([
+              new MessageButton().setStyle("SUCCESS").setLabel("Yes").setCustomId("yes"),
+              new MessageButton().setStyle("DANGER").setLabel("No").setCustomId("no"),
+            ])],
+            fetchReply: true,
+          }) as Message;
+
+          const collector = reply.createMessageComponentCollector({
+            filter: (i) => i.message.id === reply.id && i.user.id === int.user.id,
+            componentType: "BUTTON",
+            time: 10000,
+          });
+
+          collector.on("collect", async (i) => {
+            switch (i.customId) {
+              case "yes": {
+                const req = (
+                  await axios.patch(
+                      `https://api.safecord.xyz/mongo/guild/${int.guild.id}`,
+                      {
+                        verificationRole: role.id,
+                      },
+                      {
+                        headers: {
+                          "Content-Type": "application/json",
+                          "authorization": process.env.BACKEND_API_KEY ?? "89aLG9EEsWKgTzZio1ZW",
+                        },
+                      },
+                  )
+                ).data;
+
+                if (req.success) {
+                  i.update({
+                    content: `Verification role set to ${role.toString()}`,
+                    allowedMentions: {parse: []},
+                    components: [],
+                  });
+                } else {
+                  i.update({
+                    content: `An error occurred while setting the verification role.`,
+                    components: [],
+                  });
+                }
+                break;
+              }
+              case "no": {
+                i.update({
+                  content: "Please re-run the command with another role.",
+                  components: [],
+                });
+              }
+            }
+          });
+
+          collector.on("end", (collected) => {
+            if (collected.size === 0) {
+              reply.edit({
+                content: "Timed out.",
+                components: [],
+              });
+            }
+          });
+
+          return;
+        }
+
+        const req = (
+          await axios.patch(
+              `https://api.safecord.xyz/mongo/guild/${int.guild.id}`,
+              {
+                verificationRole: role.id,
+              },
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                  "authorization": process.env.BACKEND_API_KEY ?? "89aLG9EEsWKgTzZio1ZW",
+                },
+              },
+          )
+        ).data;
 
         if (req.success) {
-          return i.reply({
+          return int.reply({
             content: `Verification role set to ${role.toString()}`,
             allowedMentions: {parse: []},
           });
         } else {
-          return i.reply(req.error);
+          return int.reply(req.error);
         }
       }
     }
