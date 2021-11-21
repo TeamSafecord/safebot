49e9c1afbdf35a080773f6ee36dc2309f2fb1f56
Nick <42541753+Nciklol@users.noreply.github.com>
2021-11-20 21:33:39 -0600

    feat: store message id

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { CommandInteraction, MessageActionRow, MessageButton } from "discord.js";
 import SlashCommand from "../SlashCommand";
 import Utils from "../utils";
@@ -27,12 +28,21 @@ export default class SetupCommand extends SlashCommand {
 
     const row = new MessageActionRow().addComponents(button);
 
-    await i.channel.send({
+    const msg = await i.channel.send({
       embeds: [this.embed.setDescription("Welcome to the server! Verify by clicking the button below!")],
       components: [row],
     });
 
-    // TODO: Store the message in the database through a post request to the backend for later editing.
+    const res = await axios.patch(`https://api.safecord.xyz/mongo/guilds/${i.guild.id}`, { verificationMessageId: msg.id }, {
+      headers: {
+        "Content-Type": "application/json",
+        "authorization": process.env.BACKEND_API_KEY ?? "89aLG9EEsWKgTzZio1ZW",
+      },
+    }).catch(() => console.warn("fuck eslint"));
+
+    if (!res) {
+      return i.editReply(`Hmmm.. Something went wrong. Please report this in the [support](https://discord.gg/r5jF68pdHd) server.`);
+    }
 
     return i.editReply(
       `Sucessfully setup! Make sure to checkout [this](https://safecord.xyz/servers/${i.guild.id}) link for customizing this message!`
