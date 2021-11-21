031190ca6f7154bdeece15caccd26eddfb0bbbb2
Nick <42541753+Nciklol@users.noreply.github.com>
2021-11-21 14:44:52 -0600

    feat: send embed to db, require a role for setup

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -1,17 +1,27 @@
 import axios from "axios";
-import { CommandInteraction, MessageActionRow, MessageButton } from "discord.js";
+import { CommandInteraction, MessageActionRow, MessageButton, Role } from "discord.js";
 import SlashCommand from "../SlashCommand";
 import Utils from "../utils";
 
 export default class SetupCommand extends SlashCommand {
   constructor() {
-    super("setup", "Quickly setup your server!");
+    super("setup", "Quickly setup your server!", [
+      {
+        name: "role",
+        description: "Role to use for verification",
+        type: "ROLE",
+        required: true,
+      },
+    ]);
   }
 
   public async exec(i: CommandInteraction) {
     const member = await Utils.getMember(i);
 
     if (!member) return; // silent return because the utils function handles it
+
+    const role = i.options.getRole("role", true) as Role;
+
 
     if (!member.permissions.has("MANAGE_GUILD")) {
       return i.reply({ content: "You need the permission `Manage Server` to use this command!", ephemeral: true });
@@ -33,7 +43,15 @@ export default class SetupCommand extends SlashCommand {
       components: [row],
     });
 
-    const res = await axios.patch(`https://api.safecord.xyz/mongo/guilds/${i.guild.id}`, { verificationMessageId: msg.id }, {
+    const res = await axios.patch(`https://api.safecord.xyz/mongo/guilds/${i.guild.id}`, { verificationMessageId: msg.id, 
+      verificationEmbed: {
+        title: this.embed.title, 
+        description: this.embed.description, 
+        footer: this.embed.footer, 
+        color: this.embed.hexColor,
+      },
+      verificationRole: role.id,
+    }, {
       headers: {
         "Content-Type": "application/json",
         "authorization": process.env.BACKEND_API_KEY ?? "89aLG9EEsWKgTzZio1ZW",
@@ -45,7 +63,7 @@ export default class SetupCommand extends SlashCommand {
     }
 
     return i.editReply(
-      `Sucessfully setup! Make sure to checkout [this](https://safecord.xyz/servers/${i.guild.id}) link for customizing this message!`
+      `Sucessfully setup! Members will now recieve the ${role} role when they verify by clicking the link below! Make sure to checkout [this](https://safecord.xyz/servers/${i.guild.id}) link for customizing this message!`
     );
   }
 }
